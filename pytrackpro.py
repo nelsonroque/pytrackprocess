f5df68976e8c4da599e5bf2f435fe26fbff6f94f
Nelson Roque <nelsonroquejr@gmail.com>
2017-05-22 23:56:14 -0400

    lots more data

--- pytrackpro.py
+++ pytrackpro.py
@@ -1,16 +1,17 @@
 # libraries
 import time
+import glob
 
 # define function to read baseline data
-def readBaseline(user_file,startFlag,endFlag,section,filestring,inType,outType):
+def readBaseline(data_path,user_file,startFlag,endFlag,section,filestring,inType,outType):
     start_time = time.time()
 
     # define eyetracking messages to ignore
-    TRACK_MSGS = ['SSACC','ESACC','SFIX','EFIX','SBLINK','EBLINK','END']
+    TRACK_MSGS = ['SSACC','ESACC','SFIX','EFIX','SBLINK','EBLINK','END','MSG']
 
     # while the asc file has data to read
-    input_filename = (user_file+inType)
-    output_filename = (user_file+"_"+filestring+outType)
+    input_filename = (data_path+user_file+inType)
+    output_filename = (data_path+user_file+"_"+filestring+outType)
 
     # open output file
     df = open((output_filename),'a')
@@ -34,19 +35,35 @@ def readBaseline(user_file,startFlag,endFlag,section,filestring,inType,outType):
                     line_data = line.split("\t")
                     FRAME = line_data[0]
                     X_COORD = line_data[1]
-                    #Y_COORD = line_data[2]
-                    #PUPIL_SIZE = line_data[3]
-                    #TRIAL_DATA = [SECTION,FRAME,X_COORD,Y_COORD,PUPIL_SIZE]
-                    #df.write(",".join(TRIAL_DATA)+"\n")
+                    Y_COORD = line_data[2]
+                    PUPIL_SIZE = line_data[3]
+                    TRIAL_DATA = [SECTION,FRAME,X_COORD,Y_COORD,PUPIL_SIZE]
+                    df.write(",".join(TRIAL_DATA)+"\n")
 
     df.close()
     elapsed = time.time() - start_time
-    print(elapsed, " seconds")
+    print("SESSION TOTAL PROCESSING TIME:",elapsed,"seconds")
+# [END_FUNCTION] -------------------------------------------------------
+
+def processParticipant(data_path,user_file):
+    start_time = time.time()
+    readBaseline(data_path,user_file,"start_baseline_1","stop_baseline_1","BASELINE_1","B1",".asc",".csv")
+    readBaseline(data_path,user_file,"start_baseline_2","stop_baseline_2","BASELINE_2","B2",".asc",".csv")
+    elapsed = time.time() - start_time
+    print("PARTICIPANT TOTAL PROCESSING TIME:",elapsed,"seconds")
 # [END_FUNCTION] -------------------------------------------------------
 
 # for each participant
 # create filename
-user_file = 'sub_2015'
+data_path = 'data/'
 
-readBaseline(user_file,"start_baseline_1","stop_baseline_1","BASELINE_1","B1",".asc",".csv")
-readBaseline(user_file,"start_baseline_2","stop_baseline_2","BASELINE_2","B2",".asc",".csv")
+# glob search construction
+glob_search = data_path + "*.asc"
+
+# search for all asc files
+for name in glob.glob(glob_search):
+    filename = name
+    user_file_info = name.split("data\\")[1].split(".asc")[0]
+
+    # process participant
+    processParticipant(data_path,user_file_info)
