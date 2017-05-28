03d01ea2c898b8ef875cf8c7d67f0df79b34402f
Nelson Roque <nelsonroquejr@gmail.com>
2017-05-28 11:20:19 -0400

    updated trial report generator with list instead of individual flags

--- pyeye/reports/trial.py
+++ pyeye/reports/trial.py
@@ -23,181 +23,236 @@
 # define function to read all data (at the frame level, between two MSG flags)
 # vars: frame, x, y, pupil
 # ----------------------------------------------------------------------
-def single_2sentinels(data_path,user_file,startFlag,endFlag,filestring,outType):
+def single(data_path,user_file,flags,filestring,outType):
     import time
     import glob
     import re
 
-    start_time = time.time()
+    if(len(flags) == 2):
+        start_time = time.time()
 
-    # define eyetracking messages to ignore
-    TRACK_MSGS = ['SSACC','ESACC','SFIX','EFIX','SBLINK','EBLINK','END','MSG']
+        startFlag = flags[0]
+        endFlag = flags[1]
 
-    # while the asc file has data to read
-    input_filename = (data_path+user_file+".asc")
-    output_filename = (data_path+user_file+"_"+filestring+outType)
+        # define eyetracking messages to ignore
+        TRACK_MSGS = ['SSACC','ESACC','SFIX','EFIX','SBLINK','EBLINK','END','MSG']
 
-    # open output file
-    df = open((output_filename),'w')
+        # while the asc file has data to read
+        input_filename = (data_path+user_file+".asc")
+        output_filename = (data_path+user_file+"_"+filestring+outType)
 
-    # open the input file for reading
-    with open(input_filename,'r') as f:
-        # read each line
-        for line in f:
-            # get line where baseline starts (pre or post)
-            if(startFlag in line):
-                SECTION = filestring
-                break
-        for line in f:
-            # get line where baseline ends (pre or post)
-            if(endFlag in line):
-                break
-            else:
-                # get eye movements/pupil until end of baseline, #ignorind eyetracking messages
-                if not any(x in line for x in TRACK_MSGS):
-                    myline = line.replace('\n', ' ')
-                    myline = myline.replace(' ', '')
-                    line_data = myline.split("\t")
-                    FRAME = line_data[0]
-                    if re.match("^[0-9]+$",FRAME) is not None:
-                        if(len(line_data) < 5):
-                            #print(line_data)
-                            #print(len(line_data))
-                            print("TRACKING LOST")
-                            #print(line_data)
-                        else:
-                            X_COORD = line_data[1]
-                            if re.match("^\d+?\.\d+?$", X_COORD) is not None:
-                                Y_COORD = line_data[2]
-                                PUPIL_SIZE = line_data[3]
-                                TRIAL_DATA = [str(SECTION),FRAME,X_COORD,Y_COORD,PUPIL_SIZE]
-                                df.write(",".join(TRIAL_DATA)+"\n")
+        # open output file
+        df = open((output_filename),'w')
 
-    df.close()
-    elapsed = time.time() - start_time
-    print("(GENERIC REPORT) TOTAL PROCESSING TIME:",elapsed,"seconds")
-# [END_FUNCTION] -------------------------------------------------------
-
-# [START_FUNCTION] -----------------------------------------------------
-# define function to read all data (at the frame level, between two MSG flags)
-# vars: frame, x, y, pupil
-# ----------------------------------------------------------------------
-def all_2sentinels(data_path,user_file,startFlag,endFlag,filestring,outType):
-    import time
-    import glob
-    import re
-
-    start_time = time.time()
-
-    # define eyetracking messages to ignore
-    TRACK_MSGS = ['SSACC','ESACC','SFIX','EFIX','SBLINK','EBLINK','END','MSG']
-
-    # while the asc file has data to read
-    input_filename = (data_path+user_file+'.asc')
-    output_filename = (data_path+user_file+"_"+filestring+outType)
-
-    # open output file
-    df = open((output_filename),'w')
-
-    # start event counter
-    TRIAL = 0
-
-    # open the input file for reading
-    with open(input_filename,'r') as f:
-        # read each line
-        # search for trial start
-        SECTION='PREPARE'
-        startFound=False
-        for line in f:
-            # get line where baseline starts (pre or post)
-            if(startFlag in line):
-                #print("Trial:",TRIAL)
-                startFound = True
-                SECTION=TRIAL
-            else:
+        # open the input file for reading
+        with open(input_filename,'r') as f:
+            # read each line
+            for line in f:
+                # get line where baseline starts (pre or post)
+                if(startFlag in line):
+                    SECTION = filestring
+                    break
+            for line in f:
+                # get line where baseline ends (pre or post)
                 if(endFlag in line):
-                    startFound = False
+                    break
+                else:
+                    # get eye movements/pupil until end of baseline, #ignorind eyetracking messages
+                    if not any(x in line for x in TRACK_MSGS):
+                        myline = line.replace('\n', ' ')
+                        myline = myline.replace(' ', '')
+                        line_data = myline.split("\t")
+                        FRAME = line_data[0]
+                        if re.match("^[0-9]+$",FRAME) is not None:
+                            if(len(line_data) < 5):
+                                #print(line_data)
+                                #print(len(line_data))
+                                print("TRACKING LOST")
+                                #print(line_data)
+                            else:
+                                X_COORD = line_data[1]
+                                if re.match("^\d+?\.\d+?$", X_COORD) is not None:
+                                    Y_COORD = line_data[2]
+                                    PUPIL_SIZE = line_data[3]
+                                    TRIAL_DATA = [str(SECTION),FRAME,X_COORD,Y_COORD,PUPIL_SIZE]
+                                    df.write(",".join(TRIAL_DATA)+"\n")
+
+        df.close()
+        elapsed = time.time() - start_time
+        print("(GENERIC REPORT) TOTAL PROCESSING TIME:",elapsed,"seconds")
+    elif(len(flags) == 1):
+        start_time = time.time()
+
+        startFlag = flags[0]
+
+        # define eyetracking messages to ignore
+        TRACK_MSGS = ['SSACC','ESACC','SFIX','EFIX','SBLINK','EBLINK','MSG','END']
+
+        # while the asc file has data to read
+        input_filename = (data_path+user_file+'.asc')
+        output_filename = (data_path+user_file+"_"+filestring+outType)
+
+        # open output file
+        df = open((output_filename),'w')
+
+        # start event counter
+        TRIAL = 0
+
+        # open the input file for reading
+        with open(input_filename,'r') as f:
+            # read each line
+            # search for trial start
+            for line in f:
+                # get line where baseline starts (pre or post)
+                if(startFlag in line):
+                    SECTION=TRIAL
                     TRIAL += 1
                 else:
-                    if(startFound):
-                        if not any(x in line for x in TRACK_MSGS):
-                            myline = line.replace('\n', ' ')
-                            myline = myline.replace(' ', '')
-                            line_data = myline.split("\t")
-                            FRAME = line_data[0]
-                            if re.match("^[0-9]+$",FRAME) is not None:
-                                if(len(line_data) < 5):
-                                    #print(line_data)
-                                    #print(len(line_data))
-                                    print("TRACKING LOST")
-                                    #print(line_data)
-                                else:
-                                    X_COORD = line_data[1]
-                                    if re.match("^\d+?\.\d+?$", X_COORD) is not None:
-                                        Y_COORD = line_data[2]
-                                        PUPIL_SIZE = line_data[3]
-                                        TRIAL_DATA = [str(SECTION),FRAME,X_COORD,Y_COORD,PUPIL_SIZE]
-                                        df.write(",".join(TRIAL_DATA)+"\n")
+                    if not any(x in line for x in TRACK_MSGS):
+                        myline = line.replace('\n', ' ')
+                        myline = myline.replace(' ', '')
+                        line_data = myline.split("\t")
+                        FRAME = line_data[0]
+                        if re.match("^[0-9]+$",FRAME) is not None:
+                            if(len(line_data) < 5):
+                                #print(line_data)
+                                #print(len(line_data))
+                                print("TRACKING LOST")
+                                #print(line_data)
+                            else:
+                                X_COORD = line_data[1]
+                                if re.match("^\d+?\.\d+?$", X_COORD) is not None:
+                                    Y_COORD = line_data[2]
+                                    PUPIL_SIZE = line_data[3]
+                                    TRIAL_DATA = [str(SECTION),FRAME,X_COORD,Y_COORD,PUPIL_SIZE]
+                                    df.write(",".join(TRIAL_DATA)+"\n")
 
-    df.close()
-    elapsed = time.time() - start_time
-    print("(TRIAL SAMPLES REPORT) TOTAL PROCESSING TIME:",elapsed,"seconds")
+        df.close()
+        elapsed = time.time() - start_time
+        print("(TRIAL SAMPLES REPORT) TOTAL PROCESSING TIME:",elapsed,"seconds")
+    else:
+        print("ERROR: Trial Start Flag required. Trial End Flag optional. Both must be strings. Spaces can be included if part of uniquely identifying the flag.")
 # [END_FUNCTION] -------------------------------------------------------
 
 # [START_FUNCTION] -----------------------------------------------------
 # define function to read all data (at the frame level, between two MSG flags)
 # vars: frame, x, y, pupil
 # ----------------------------------------------------------------------
-def all_1sentinel(data_path,user_file,startFlag,filestring,outType):
+def all(data_path,user_file,flags,filestring,outType):
     import time
     import glob
     import re
 
-    start_time = time.time()
+    if(len(flags) == 2):
 
-    # define eyetracking messages to ignore
-    TRACK_MSGS = ['SSACC','ESACC','SFIX','EFIX','SBLINK','EBLINK','MSG','END']
+        start_time = time.time()
 
-    # while the asc file has data to read
-    input_filename = (data_path+user_file+'.asc')
-    output_filename = (data_path+user_file+"_"+filestring+outType)
+        startFlag = flags[0]
+        endFlag = flags[1]
 
-    # open output file
-    df = open((output_filename),'w')
+        # define eyetracking messages to ignore
+        TRACK_MSGS = ['SSACC','ESACC','SFIX','EFIX','SBLINK','EBLINK','END','MSG']
 
-    # start event counter
-    TRIAL = 0
+        # while the asc file has data to read
+        input_filename = (data_path+user_file+'.asc')
+        output_filename = (data_path+user_file+"_"+filestring+outType)
 
-    # open the input file for reading
-    with open(input_filename,'r') as f:
-        # read each line
-        # search for trial start
-        for line in f:
-            # get line where baseline starts (pre or post)
-            if(startFlag in line):
-                SECTION=TRIAL
-                TRIAL += 1
-            else:
-                if not any(x in line for x in TRACK_MSGS):
-                    myline = line.replace('\n', ' ')
-                    myline = myline.replace(' ', '')
-                    line_data = myline.split("\t")
-                    FRAME = line_data[0]
-                    if re.match("^[0-9]+$",FRAME) is not None:
-                        if(len(line_data) < 5):
-                            #print(line_data)
-                            #print(len(line_data))
-                            print("TRACKING LOST")
-                            #print(line_data)
-                        else:
-                            X_COORD = line_data[1]
-                            if re.match("^\d+?\.\d+?$", X_COORD) is not None:
-                                Y_COORD = line_data[2]
-                                PUPIL_SIZE = line_data[3]
-                                TRIAL_DATA = [str(SECTION),FRAME,X_COORD,Y_COORD,PUPIL_SIZE]
-                                df.write(",".join(TRIAL_DATA)+"\n")
+        # open output file
+        df = open((output_filename),'w')
 
-    df.close()
-    elapsed = time.time() - start_time
-    print("(TRIAL SAMPLES REPORT) TOTAL PROCESSING TIME:",elapsed,"seconds")
+        # start event counter
+        TRIAL = 0
+
+        # open the input file for reading
+        with open(input_filename,'r') as f:
+            # read each line
+            # search for trial start
+            SECTION='PREPARE'
+            startFound=False
+            for line in f:
+                # get line where baseline starts (pre or post)
+                if(startFlag in line):
+                    #print("Trial:",TRIAL)
+                    startFound = True
+                    SECTION=TRIAL
+                else:
+                    if(endFlag in line):
+                        startFound = False
+                        TRIAL += 1
+                    else:
+                        if(startFound):
+                            if not any(x in line for x in TRACK_MSGS):
+                                myline = line.replace('\n', ' ')
+                                myline = myline.replace(' ', '')
+                                line_data = myline.split("\t")
+                                FRAME = line_data[0]
+                                if re.match("^[0-9]+$",FRAME) is not None:
+                                    if(len(line_data) < 5):
+                                        #print(line_data)
+                                        #print(len(line_data))
+                                        print("TRACKING LOST")
+                                        #print(line_data)
+                                    else:
+                                        X_COORD = line_data[1]
+                                        if re.match("^\d+?\.\d+?$", X_COORD) is not None:
+                                            Y_COORD = line_data[2]
+                                            PUPIL_SIZE = line_data[3]
+                                            TRIAL_DATA = [str(SECTION),FRAME,X_COORD,Y_COORD,PUPIL_SIZE]
+                                            df.write(",".join(TRIAL_DATA)+"\n")
+
+        df.close()
+        elapsed = time.time() - start_time
+        print("(TRIAL SAMPLES REPORT) TOTAL PROCESSING TIME:",elapsed,"seconds")
+    elif(len(flags) == 1):
+        start_time = time.time()
+
+        startFlag = flags[0]
+
+        # define eyetracking messages to ignore
+        TRACK_MSGS = ['SSACC','ESACC','SFIX','EFIX','SBLINK','EBLINK','MSG','END']
+
+        # while the asc file has data to read
+        input_filename = (data_path+user_file+'.asc')
+        output_filename = (data_path+user_file+"_"+filestring+outType)
+
+        # open output file
+        df = open((output_filename),'w')
+
+        # start event counter
+        TRIAL = 0
+
+        # open the input file for reading
+        with open(input_filename,'r') as f:
+            # read each line
+            # search for trial start
+            for line in f:
+                # get line where baseline starts (pre or post)
+                if(startFlag in line):
+                    SECTION=TRIAL
+                    TRIAL += 1
+                else:
+                    if not any(x in line for x in TRACK_MSGS):
+                        myline = line.replace('\n', ' ')
+                        myline = myline.replace(' ', '')
+                        line_data = myline.split("\t")
+                        FRAME = line_data[0]
+                        if re.match("^[0-9]+$",FRAME) is not None:
+                            if(len(line_data) < 5):
+                                #print(line_data)
+                                #print(len(line_data))
+                                print("TRACKING LOST")
+                                #print(line_data)
+                            else:
+                                X_COORD = line_data[1]
+                                if re.match("^\d+?\.\d+?$", X_COORD) is not None:
+                                    Y_COORD = line_data[2]
+                                    PUPIL_SIZE = line_data[3]
+                                    TRIAL_DATA = [str(SECTION),FRAME,X_COORD,Y_COORD,PUPIL_SIZE]
+                                    df.write(",".join(TRIAL_DATA)+"\n")
+
+        df.close()
+        elapsed = time.time() - start_time
+        print("(TRIAL SAMPLES REPORT) TOTAL PROCESSING TIME:",elapsed,"seconds")
+    else:
+        print("ERROR: Trial Start Flag required. Trial End Flag optional. Both must be strings. Spaces can be included if part of uniquely identifying the flag.")
 # [END_FUNCTION] -------------------------------------------------------
