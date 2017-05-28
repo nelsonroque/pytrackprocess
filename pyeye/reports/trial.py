705e1ffbb271ecc8b040dc9cfafb64e6c33d2159
Nelson Roque <nelsonroquejr@gmail.com>
2017-05-28 10:58:58 -0400

    single sentinel version (v0)

--- pyeye/reports/trial.py
+++ pyeye/reports/trial.py
@@ -23,7 +23,7 @@
 # define function to read all data (at the frame level, between two MSG flags)
 # vars: frame, x, y, pupil
 # ----------------------------------------------------------------------
-def single(data_path,user_file,startFlag,endFlag,filestring,outType):
+def single_2sentinels(data_path,user_file,startFlag,endFlag,filestring,outType):
     import time
     import glob
     import re
@@ -82,7 +82,7 @@ def single(data_path,user_file,startFlag,endFlag,filestring,outType):
 # define function to read all data (at the frame level, between two MSG flags)
 # vars: frame, x, y, pupil
 # ----------------------------------------------------------------------
-def multiple(data_path,user_file,startFlag,endFlag,filestring,outType):
+def all_2sentinels(data_path,user_file,startFlag,endFlag,filestring,outType):
     import time
     import glob
     import re
@@ -138,6 +138,64 @@ def multiple(data_path,user_file,startFlag,endFlag,filestring,outType):
                                         PUPIL_SIZE = line_data[3]
                                         TRIAL_DATA = [str(SECTION),FRAME,X_COORD,Y_COORD,PUPIL_SIZE]
                                         df.write(",".join(TRIAL_DATA)+"\n")
+
+    df.close()
+    elapsed = time.time() - start_time
+    print("(TRIAL SAMPLES REPORT) TOTAL PROCESSING TIME:",elapsed,"seconds")
+# [END_FUNCTION] -------------------------------------------------------
+
+# [START_FUNCTION] -----------------------------------------------------
+# define function to read all data (at the frame level, between two MSG flags)
+# vars: frame, x, y, pupil
+# ----------------------------------------------------------------------
+def all_1sentinel(data_path,user_file,startFlag,filestring,outType):
+    import time
+    import glob
+    import re
+
+    start_time = time.time()
+
+    # define eyetracking messages to ignore
+    TRACK_MSGS = ['SSACC','ESACC','SFIX','EFIX','SBLINK','EBLINK','MSG','END']
+
+    # while the asc file has data to read
+    input_filename = (data_path+user_file+'.asc')
+    output_filename = (data_path+user_file+"_"+filestring+outType)
+
+    # open output file
+    df = open((output_filename),'w')
+
+    # start event counter
+    TRIAL = 0
+
+    # open the input file for reading
+    with open(input_filename,'r') as f:
+        # read each line
+        # search for trial start
+        for line in f:
+            # get line where baseline starts (pre or post)
+            if(startFlag in line):
+                SECTION=TRIAL
+                TRIAL += 1
+            else:
+                if not any(x in line for x in TRACK_MSGS):
+                    myline = line.replace('\n', ' ')
+                    myline = myline.replace(' ', '')
+                    line_data = myline.split("\t")
+                    FRAME = line_data[0]
+                    if re.match("^[0-9]+$",FRAME) is not None:
+                        if(len(line_data) < 5):
+                            #print(line_data)
+                            #print(len(line_data))
+                            print("TRACKING LOST")
+                            #print(line_data)
+                        else:
+                            X_COORD = line_data[1]
+                            if re.match("^\d+?\.\d+?$", X_COORD) is not None:
+                                Y_COORD = line_data[2]
+                                PUPIL_SIZE = line_data[3]
+                                TRIAL_DATA = [str(SECTION),FRAME,X_COORD,Y_COORD,PUPIL_SIZE]
+                                df.write(",".join(TRIAL_DATA)+"\n")
 
     df.close()
     elapsed = time.time() - start_time
